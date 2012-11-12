904cd9a751dd3f13cca9d2a44b033aab13bd9b8f
David Rosen <david@wolfire.com>
2012-11-12 13:13:31 -0800

    Synchronized cursor colors

--- FTJ Project/Assets/Scripts/CursorScript.cs
+++ FTJ Project/Assets/Scripts/CursorScript.cs
@@ -5,43 +5,54 @@ public class CursorScript : MonoBehaviour {
 	const float CURSOR_INERTIA = 0.001f;
 	const float HOLD_FORCE = 10000.0f;
 	const float MAX_DICE_VEL = 15.0f;
-	Vector3 pos;
-	Vector3 target_pos;
-	GameObject held;
-
+	Color color_;
+	GameObject held_;
+	
+	void SetColor(Vector3 color){
+		color_ = new Color(color.x, color.y, color.z);
+		Transform pointer = transform.FindChild("Pointer");
+		Transform pointer_tint = pointer.FindChild("pointer_tint");
+		Transform default_obj = pointer_tint.FindChild("default");
+		default_obj.renderer.material.color = color_;	
+	}
+	
 	void Start () {
-		renderer.material.color = new Color(Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),1.0f);
+		if(networkView.isMine){
+			SetColor(new Vector3(Random.Range(0.0f,1.0f),
+								 Random.Range(0.0f,1.0f),
+								 Random.Range(0.0f,1.0f)));
+		}
 	}
 	
 	void Update () {
 		if(networkView.isMine){
+			Vector3 pos = new Vector3();
 			{
 				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 				RaycastHit raycast_hit = new RaycastHit();
 				if(Physics.Raycast(ray, out raycast_hit, 100.0f, 1 << 8)){
-					target_pos = raycast_hit.point - ray.direction;
+					pos = raycast_hit.point - ray.direction;
 				}
 			}
-			pos = target_pos;
 			
-			if(Input.GetMouseButtonDown(0)){
+			if(Input.GetMouseButtonDown(0) && Network.isServer){
 				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 				RaycastHit raycast_hit = new RaycastHit();
 				if(Physics.Raycast(ray, out raycast_hit)){
 					if(raycast_hit.collider.gameObject.layer == 10){
 						//ConsoleScript.Log ("Clicked on die");
-						held = raycast_hit.collider.gameObject;
+						held_ = raycast_hit.collider.gameObject;
 					}
 				}
 			}
 			if(Input.GetMouseButtonUp(0)){
-				if(held){
-					if(held.rigidbody.velocity.magnitude > MAX_DICE_VEL){
-						held.rigidbody.velocity = held.rigidbody.velocity.normalized * MAX_DICE_VEL;
+				if(held_){
+					if(held_.rigidbody.velocity.magnitude > MAX_DICE_VEL){
+						held_.rigidbody.velocity = held_.rigidbody.velocity.normalized * MAX_DICE_VEL;
 					}
-					held.rigidbody.angularVelocity = new Vector3(Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f) * 100.0f);			
+					held_.rigidbody.angularVelocity = new Vector3(Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f) * 100.0f);			
 				
-					held = null;
+					held_ = null;
 				}
 			}		
 			rigidbody.position = pos;
@@ -51,12 +62,29 @@ public class CursorScript : MonoBehaviour {
 	
 	void FixedUpdate() {
 		if(networkView.isMine){
-			if(held){
-				held.rigidbody.AddForce((pos - held.rigidbody.position) * Time.deltaTime * HOLD_FORCE);
-				held.rigidbody.velocity *= 0.8f;			
-				held.rigidbody.angularVelocity *= 0.9f;			
-				held.rigidbody.WakeUp();
+			if(held_){
+				held_.rigidbody.AddForce((transform.position - held_.rigidbody.position) * Time.deltaTime * HOLD_FORCE);
+				held_.rigidbody.velocity *= 0.8f;			
+				held_.rigidbody.angularVelocity *= 0.9f;			
+				held_.rigidbody.WakeUp();
 			}
+		}
+	}
+	
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		// Send data to server
+		if (stream.isWriting)
+		{
+			Vector3 color = new Vector3(color_.r, color_.g, color_.b);
+			stream.Serialize(ref color);
+		}
+		// Read data from remote client
+		else
+		{
+			Vector3 color = new Vector3();
+			stream.Serialize(ref color);
+			SetColor(color);
 		}
 	}
 }
