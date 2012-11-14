b9f35e2611c21dd05e3f943e6dfe5e36f8c7ae50
David Rosen <david@wolfire.com>
2012-11-14 12:13:20 -0800

    Progress on card creation

--- FTJ Project/Assets/Scripts/CardCreationScript.cs
+++ FTJ Project/Assets/Scripts/CardCreationScript.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class CardCreationScript : MonoBehaviour {
-
+	public Texture2D[] back_textures;
+	enum BackID{DUNGEON_ONE = 0, DUNGEON_TWO = 1, DUNGEON_THREE = 2, HERO = 3, NATURE = 4, TOWN = 5};
+	
 	// Use this for initialization
 	void Start () {
 		var title_text = transform.FindChild("Title").GetComponent<TextMesh>();
@@ -13,6 +15,8 @@ public class CardCreationScript : MonoBehaviour {
 		rules_text.text = "On 1 or 2: skip a turn\nOn 5 or 6: draw four, pick one";
 		var flavour_text = transform.FindChild("Flavour").GetComponent<TextMesh>();
 		flavour_text.text = "\"I hate spiders!\" -Some dude";
+		var card_back = transform.FindChild("Back").transform.FindChild("default");
+		card_back.renderer.material.mainTexture = back_textures[(int)BackID.DUNGEON_ONE];
 	}
 	
 	// Update is called once per frame
