3c004ff49e09e194ca0415e27380558cab0f1127
David Rosen <david@wolfire.com>
2012-11-14 11:31:05 -0800

    Increased dice gravity

--- FTJ Project/Assets/Scripts/ZoomCameraScript.cs
+++ FTJ Project/Assets/Scripts/ZoomCameraScript.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class ZoomCameraScript : MonoBehaviour {
-
+	Quaternion old_rotation;
+	const float ZOOM_INERTIA = 0.0001f;
+	
 	// Use this for initialization
 	void Start () {
-	
+		old_rotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
@@ -14,7 +16,9 @@ public class ZoomCameraScript : MonoBehaviour {
 		if(board_script){
 			var cursor = board_script.GetMyCursorObject();
 			if(cursor){
+				old_rotation = transform.rotation;
 				transform.LookAt(cursor.transform.position, GameObject.Find("Main Camera").transform.up);
+				transform.rotation = Quaternion.Lerp(transform.rotation, old_rotation, Mathf.Pow(ZOOM_INERTIA, Time.deltaTime));
 				//transform.up = GameObject.Find("Main Camera").transform.up;
 			}
 		}
