e624e6cbfa1c2300cc27879b3654326679077ca9
David Rosen <david@wolfire.com>
2012-11-14 17:08:09 -0800

    Better deck/card transition

--- FTJ Project/Assets/Scripts/DeckScript.cs
+++ FTJ Project/Assets/Scripts/DeckScript.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using MiniJSON;
 
 public class CardData {
@@ -64,32 +65,79 @@ public class DeckScript : MonoBehaviour {
 				cards_.Add(card_data);	
 			}
 		}
-		var top_transform = transform.FindChild("top_card").transform;
-		top_card_ = (GameObject)GameObject.Instantiate(card_prefab, top_transform.position, top_transform.rotation);
-		top_card_.transform.parent = transform;
-		GameObject.Destroy(top_card_.rigidbody);
-		GameObject.Destroy(top_card_.collider);
-		{
-			var card_script = top_card_.GetComponent<CardScript>();
-			card_script.SetCardData(cards_[0]);
-			card_script.Bake();
+		RegenerateEndCards();
+	}
+	
+	void RegenerateEndCards() {
+		if(!top_card_ && cards_.Count > 1){
+			var pos = transform.FindChild("bottom_card").transform.position;
+			var rot = transform.FindChild("bottom_card").transform.rotation;
+			pos += transform.rotation * new Vector3(0,cards_.Count*0.012f+0.1f,0);
+			top_card_ = CreateCard(cards_[0],pos,rot);
+		}		
+		if(!bottom_card_ && cards_.Count > 0){
+			var pos = transform.FindChild("bottom_card").transform.position;
+			var rot = transform.FindChild("bottom_card").transform.rotation;
+			bottom_card_ = CreateCard(cards_[cards_.Count-1],pos,rot);
 		}
-		
-		var bottom_transform = transform.FindChild("bottom_card").transform;
-		bottom_card_ = (GameObject)GameObject.Instantiate(card_prefab, bottom_transform.position, bottom_transform.rotation);
-		bottom_card_.transform.parent = transform;
-		GameObject.Destroy(bottom_card_.rigidbody);
-		GameObject.Destroy(bottom_card_.collider);
+	}
+	
+	GameObject CreateCard(CardData card_data, Vector3 pos, Quaternion rot){
+		var card = (GameObject)GameObject.Instantiate(card_prefab, pos, rot);
+		card.transform.parent = transform;
+		GameObject.Destroy(card.rigidbody);
+		card.collider.enabled = false;
+		var card_script = card.GetComponent<CardScript>();
+		card_script.SetCardData(card_data);
+		card_script.Bake();
+		return card;
+	}
+	
+	void CopyComponent(Component old_component, GameObject game_object){
+		Component new_component = game_object.AddComponent(old_component.GetType());
+		foreach (FieldInfo f in old_component.GetType().GetFields())
 		{
-			var card_script = bottom_card_.GetComponent<CardScript>();
-			card_script.SetCardData(cards_[cards_.Count-1]);
-			card_script.Bake();
+		  f.SetValue(new_component, f.GetValue(old_component));
 		}
-		var deck_transform = transform.FindChild("default").transform;
+	}
+	
+	public GameObject TakeTopCard(){
+		if(cards_.Count == 0){
+			return null;
+		}
+		GameObject card;
+		if(top_card_){
+			card = top_card_;
+			top_card_ = null;
+			cards_.RemoveAt(0);
+		} else {
+			card = bottom_card_;
+			bottom_card_ = null;
+			cards_.RemoveAt(cards_.Count-1);
+		}
+		if(cards_.Count <= 1){
+			transform.FindChild("default").renderer.enabled = false;
+		}
+		if(cards_.Count == 1){
+			bottom_card_.transform.position += transform.rotation * new Vector3(0,0.07f,0);
+		}
+		RegenerateEndCards();
+		CopyComponent(card_prefab.rigidbody, card);
+		card.collider.enabled = true;
+		card.transform.parent = null;
+		card.transform.localScale = new Vector3(1,1,1);
+		return card;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.localScale = new Vector3(1,cards_.Count * CARD_THICKNESS_MULT,1);	
+		transform.FindChild("default").localScale = new Vector3(1,Mathf.Max(2,cards_.Count) * CARD_THICKNESS_MULT,1);	
+		if(Input.GetMouseButtonDown(0)){
+			var card = TakeTopCard();
+			if(card){
+				card.rigidbody.AddForce(new Vector3(0,1000,0));
+				GameObject.Destroy(card);
+			}
+		}
 	}
 }
