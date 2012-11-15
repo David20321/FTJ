c6dcdfaabf024f9083e4f90515427975c35d37ed
David Rosen <david@wolfire.com>
2012-11-14 18:03:32 -0800

    Hooked up dice sounds to physics

--- FTJ Project/Assets/Scripts/DiceScript.cs
+++ FTJ Project/Assets/Scripts/DiceScript.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
 using System.Collections;
 
+
 public class DiceScript : MonoBehaviour {
 	public int id_;
 	public int held_by_player_;
 	public int owner_id_;
 	public enum Type {DIE, TOKEN};
 	public Type type_;
+	public AudioClip[] dice_impact_wood;
+	public AudioClip[] dice_impact_board;
+	public AudioClip[] dice_impact_dice;
+	float last_sound_time = 0.0f;
+	const float PHYSICS_SOUND_DELAY = 0.1f;
+	
+	void PlayRandomSound(AudioClip[] clips, float volume = 1.0f){
+		audio.PlayOneShot(clips[Random.Range(0,clips.Length)], volume);
+	}		
 	
 	void Start () {
 		BoardScript.Instance().RegisterDiceObject(gameObject);
@@ -33,6 +43,26 @@ public class DiceScript : MonoBehaviour {
 			int owner_id = -1;
 			stream.Serialize(ref owner_id);
 			owner_id_ = owner_id;
+		}
+	}
+	
+	void OnCollisionEnter(Collision info) {
+		if(type_ == Type.DIE){
+			if(info.relativeVelocity.magnitude > 1.0f && Time.time > last_sound_time + PHYSICS_SOUND_DELAY) { 
+				float volume = info.relativeVelocity.magnitude*0.1f;
+				switch(info.collider.gameObject.layer){
+					case 8:
+						PlayRandomSound(dice_impact_wood, volume);
+						break;
+					case 9:						
+						PlayRandomSound(dice_impact_board, volume);
+						break;
+					default:
+						PlayRandomSound(dice_impact_dice, volume);
+						break;
+				}
+				last_sound_time = Time.time;
+			}	
 		}
 	}
 }
