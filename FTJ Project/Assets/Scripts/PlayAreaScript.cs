6b2b422ef3d3303364655c5f4ab700b149f5fe41
David Rosen <david@wolfire.com>
2012-11-18 12:36:54 -0800

    Added play area art

--- FTJ Project/Assets/Scripts/PlayAreaScript.cs
+++ FTJ Project/Assets/Scripts/PlayAreaScript.cs
@@ -6,11 +6,13 @@ public class PlayAreaScript : MonoBehaviour {
 	public void SetColor(int id){
 		var color = ColorPalette.GetColor(id);
 		float avg_color = (color.r+color.g+color.b)/3.0f;
-		float desaturation = 0.7f;
-		color = new Color(Mathf.Lerp(color.r, avg_color, desaturation),
-						  Mathf.Lerp(color.g, avg_color, desaturation),
-						  Mathf.Lerp(color.b, avg_color, desaturation));
-		renderer.material.color = color;
+		float desaturation = 0.3f;
+		float darken = 0.8f;
+		color = new Color(Mathf.Lerp(color.r, avg_color, desaturation)*darken,
+						  Mathf.Lerp(color.g, avg_color, desaturation)*darken,
+						  Mathf.Lerp(color.b, avg_color, desaturation)*darken,
+						  0.9f);
+		transform.FindChild("Tint").FindChild("objobjBox02").renderer.material.color = color;
 		if(Network.isServer){
 			networkView.RPC("SetColor",RPCMode.OthersBuffered, id);
 		}
